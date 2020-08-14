383cbae433b8397907f3b6b26492448074ad5338
aaron <aaronlin@berkeley.edu>
2020-08-14 11:30:32 -0700

    rename

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# wifi_drone_obst_avoidance
+# Collision Avoidance for WiFi Drones
 
 Most drones with collision avoidance incorporate some degree of computer vision and artificial intelligence. This project aims to extend the autonomous and safety capabilities of smaller and more affordable consumer drones (i.e. DJI Tello ~$100) with a lightweight (about 40g), low-cost (about $40) collision avoidance system.
 
@@ -18,7 +18,7 @@ TODO
 
 ```
 sudo apt-get update
-sudo apt-get install python-pip python-dev ipython
+sudo apt-get install python-pip python-dev ipython build-essential
 
 sudo apt-get install bluetooth libbluetooth-dev
 ```
@@ -26,8 +26,6 @@ sudo apt-get install bluetooth libbluetooth-dev
 VL53L0x Python Library: https://github.com/pimoroni/VL53L0X_rasp_python
 
 ```
-
-sudo apt-get install build-essential python-dev
 cd your_git_directory
 git clone https://github.com/pimoroni/VL53L0X_rasp_python.git
 cd VL53L0X_rasp_python
@@ -36,7 +34,9 @@ make
 ```
 
 PyBluez 0.22: https://github.com/pybluez/pybluez
-(use sudo pip install pybluez==0.22)
+```
+sudo pip install pybluez=0.22
+```
 
 
 
@@ -47,7 +47,11 @@ ROS Kinetic: http://wiki.ros.org/kinetic/Installation/Ubuntu
 Tellopy (build from source): https://github.com/hanyazou/TelloPy
 
 PyBluez 0.22: https://github.com/pybluez/pybluez
-(use sudo pip install pybluez==0.22)
+```
+sudo apt-get install bluetooth libbluetooth-dev
+sudo pip install pybluez=0.22
+```
+
 
 ## Building The Repository
 
@@ -57,7 +61,7 @@ mkdir -p ~/catkin_ws_tello/src/
 
 cd ~/catkin_ws_tello/src/
 
-git clone https://github.com/KhazanahAmericasInc/wifi_drone_avoidance/
+git clone https://github.com/aaaaronlin/tello_collision_avoidance/
 
 cd ..
 
@@ -78,6 +82,8 @@ Running a script on startup:
 
 copy /board/sensor_system.py to /Desktop on PiZero
 
+change the bluetooth address to your PC address, as well as GPIO pins used
+
 ```
 cd
 sudo nano /etc/rc.local
@@ -96,3 +102,5 @@ Enable Bluetooth
 Enable WiFi (for drone AP)
 
 ## Sensor System Hub + Wiring Diagram
+
+https://www.instructables.com/id/VL53L0X-Sensor-System/
--- board/sensor_system.py
+++ board/sensor_system.py
@@ -1,87 +1,126 @@
-#!/usr/bin/python
-
-# MIT License
-# 
-# Copyright (c) 2017 John Bryan Moore
-# 
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-# 
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
+import bluetooth
 import time
 import VL53L0X
 import RPi.GPIO as GPIO
 
-class Sensor_System():
+server_address = "94:E9:79:E6:E1:90"
+port = 3
 
-    def __init__(self):
-        self.s1 = None
+S1_SHDN = 14  # forward
+S2_SHDN = 22  # left
+S3_SHDN = 21  # right
+S4_SHDN = 20  # up
 
-        self.s2 = None
-        # GPIO for Sensor 1 shutdown pin
-        self.s1_SHDN = 20
-        # GPIO for Sensor 2 shutdown pin
-        self.s2_SHDN = 16
+GPIO.setwarning(False)
 
-        self.rate = 0.0
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(S1_SHDN, GPIO.OUT)
+GPIO.setup(S2_SHDN, GPIO.OUT)
+GPIO.setup(S3_SHDN, GPIO.OUT)
+GPIO.setup(S4_SHDN, GPIO.OUT)
 
-        GPIO.setwarnings(False)
+GPIO.setup(S1_SHDN, GPIO.LOW)
+GPIO.setup(S2_SHDN, GPIO.LOW)
+GPIO.setup(S3_SHDN, GPIO.LOW)
+GPIO.setup(S4_SHDN, GPIO.LOW)
 
-        GPIO.setmode(GPIO.BCM)
+GPIO.output(S1_SHDN, False)
+GPIO.output(S2_SHDN, False)
+GPIO.output(S3_SHDN, False)
+GPIO.output(S4_SHDN, False)
 
-    def reset(self):
-        # Setup GPIO for shutdown pins on each VL53L0X
+GPIO.output(S1_SHDN, True)
+time.sleep(0.5)
+s1 = VL53L0X.VL53L0X(i2c_address=0x29)
+s1.open()
+s1.close()
+s1.change_address(0x2A)
+s1.open()
 
-        GPIO.setup(s1_SHDN, GPIO.OUT)
-        GPIO.setup(s2_SHDN, GPIO.OUT)
+GPIO.output(S2_SHDN, True)
+time.sleep(0.5)
+s2 = VL53L0X.VL53L0X(i2c_address=0x29)
+s2.open()
+s2.close()
+s2.change_address(0x2B)
+s2.open()
 
-        # Set all shutdown pins low to turn off each VL53L0X
-        GPIO.output(s1_SHDN, GPIO.LOW)
-        GPIO.output(s2_SHDN, GPIO.LOW)
+GPIO.output(S1_SHDN, True)
+time.sleep(0.5)
+s3 = VL53L0X.VL53L0X(i2c_address=0x29)
+s3.open()
+s3.close()
+s3.change_address(0x2C)
+s3.open()
 
-        # Keep all low for 500 ms or so to make sure they reset
-        time.sleep(0.50)
+GPIO.output(S1_SHDN, True)
+time.sleep(0.5)
+s4 = VL53L0X.VL53L0X(i2c_address=0x29)
+s4.open()
+s4.close()
+s4.change_address(0x2D)
+s4.open()
 
-    def start(self):
+connected = False
+ranging = False
+i = 0
 
-        # Create one object per VL53L0X passing the address to give to
-        # each.
-        self.s1 = VL53L0X.VL53L0X(address=0x2B)
-        self.s2 = VL53L0X.VL53L0X(address=0x2D)
+while True:
+	if not connected:
+		s = None
+		s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+		try:
+			print("Connecting...")
+			s.connect((server_address, port))
+			print("Connected")
+			i = 0
+			connected = True
+		except bluetooth.btcommon.BluetoothError as btErr:
+			s.close()
+			connected = False
+			time.sleep(1)
+			continue
+	if connected:
+		if not ranging:
+			print("start ranging")
+			time.sleep(0.5)
+			s1.start_ranging(VL53L0X.Vl53l0xAccuracyMode.LONG_RANGE)
+			time.sleep(0.5)
+			s2.start_ranging(VL53L0X.Vl53l0xAccuracyMode.LONG_RANGE)
+			time.sleep(0.5)
+			s3.start_ranging(VL53L0X.Vl53l0xAccuracyMode.LONG_RANGE)
+			time.sleep(0.5)
+			s4.start_ranging(VL53L0X.Vl53l0xAccuracyMode.LONG_RANGE)
+			ranging = True
 
-        # Set shutdown pin high for the first VL53L0X then 
-        # call to start ranging 
-        GPIO.output(sensor1_shutdown, GPIO.HIGH)
-        time.sleep(0.50)
-        self.s1.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+		d1 = s1.get_distance()
+		d2 = s2.get_distance()
+		d3 = s3.get_distance()
+		d4 = s4.get_distance()
 
-        # Set shutdown pin high for the second VL53L0X then 
-        # call to start ranging 
-        GPIO.output(sensor2_shutdown, GPIO.HIGH)
-        time.sleep(0.50)
-        self.s2.start_ranging(VL53L0X.VL53L0X_BETTER_ACCURACY_MODE)
+		msg = str(i) + "," + str(d1) + "," + str(d2) + "," + str(d3) + "," + str(d4)
+		print(msg)
 
-        self.rate = s1.get_timing()/1000000.00
+		try:
+			s.send(msg)
+		except bluetooth.btcommon.BluetoothError as btErr:
+			print(btErr)
+			s.close()
+			connected = False
 
-    def poll_meas(self):
-        return [self.s1.get_distance(), self.s2.get_distance()] # in mm
+			s1.stop_ranging()
+			s2.stop_ranging()
+			s3.stop_ranging()
+			s4.stop_ranging()
 
-    def end(self):
-        self.s1.stop_ranging()
-        GPIO.output(sensor2_shutdown, GPIO.LOW)
-        self.s2.stop_ranging()
-        GPIO.output(sensor1_shutdown, GPIO.LOW)
+			ranging = False
+
+			continue
+		i += 1
+		time.sleep(0.033)
+
+s.close()
+s1.close()
+s2.close()
+s3.close()
+s4.close()
